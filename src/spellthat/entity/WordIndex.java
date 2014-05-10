aa1be04002e9785306eb432cc6f2e1819f5dff1e
oservieres <oservieres@mediastay.com>
2014-05-10 15:39:37 +0200

    Add: new theme and better letter recognition

--- src/spellthat/entity/WordIndex.java
+++ src/spellthat/entity/WordIndex.java
@@ -1,5 +1,6 @@
 package spellthat.entity;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -10,20 +11,27 @@ public class WordIndex {
 
 	public void add(Character letter, String word) {
 		ArrayList<String> list = null;
-		list = map.get(Character.toLowerCase(letter));
+		Character slug = slugify(letter);
+		list = map.get(slug);
 		if (null == list) {
 			list = new ArrayList<String>();
-			map.put(Character.toLowerCase(letter), list);
+			map.put(slug, list);
 		}
 		list.add(word);
 	}
 	
 	public String getWordByLetter(Character letter) {
-		ArrayList<String> wordsList = map.get(Character.toLowerCase(letter));
+		ArrayList<String> wordsList = map.get(slugify(letter));
 		if (null == wordsList) {
 			return "n'importe quoi";
 		}
 		Collections.shuffle(wordsList);
 		return wordsList.get(0);
+	}
+	
+	public Character slugify(Character letter) {
+		String slug = Normalizer.normalize(Character.toString(letter), Normalizer.Form.NFD);
+	    slug = slug.replaceAll("[^\\p{ASCII}]", "");
+		return Character.toLowerCase(slug.charAt(0));
 	}
 }
